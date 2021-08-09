9daec4bf796500e0ccbbd02fb5d62e4aa9e9bae7
riflockle7 <ridickle7@gmail.com>
2021-08-10 02:24:03 +0900

    #1 Spring Boot View 설정 및 JSP 연동하기(Thymeleaf 추가)
    link : https://goddaehee.tistory.com/204
    notion : https://www.notion.so/individualabcd/Spring-83fa3b10dbd34207b405709756c0ef19

--- src/main/java/com/riflockle7/demo/controller/DemoController.java
+++ src/main/java/com/riflockle7/demo/controller/DemoController.java
@@ -1,16 +1,22 @@
 package com.riflockle7.demo.controller;
 
+import com.riflockle7.demo.vo.TestVo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class DemoController {
     @RequestMapping(value = "/home")
     public String home() {
-        return "index.html";
+        return "index";
     }
 
     @ResponseBody
@@ -19,9 +25,30 @@ class DemoController {
         String value = "테스트 String";
         return value;
     }
+
+    @RequestMapping("/test")
+    public ModelAndView test() throws Exception {
+        ModelAndView mav = new ModelAndView("demo");
+        mav.addObject("name", "rickkky");
+        List<String> testList = new ArrayList<>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+        mav.addObject("list", testList);
+        return mav;
+    }
+
+    @RequestMapping("/thymeleafTest")
+    public String thymeleafTest(Model model) {
+        TestVo testModel = new TestVo("rickky", "맄희");
+        model.addAttribute("testModel", testModel);
+        return "thymeleaf/thymeleafTest";
+    }
 }
 
-/** [@Controller], [@ResponseBody] 를 합친 역할 */
+/**
+ * [@Controller], [@ResponseBody] 를 합친 역할
+ */
 @RestController
 class DemoRestController {
     @RequestMapping(value = "/testValue", method = RequestMethod.GET)
